85ebd553815c867cb25e5d059751e675bd32737c
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-03-25 14:35:42 -0600

    Clase 1
    Base

---
+++ Clase 1/src/clase/pkg1/Clase1.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg1;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Rodrigo
+ */
+
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       //Mostrar un mensaje en consola
+       //System.out.println("Hola mundo");
+       //Tipos de variables
+       /*
+       int=enteros
+       float=decimales
+       double=decimales
+       char=caracteres
+       String=Cadenas de caracteres
+       boolean=valores booleanos
+       */
+       /*int Entero=12;
+       float Decimal1=(float)12.22;
+       double Decimal2=12.22;
+       char caracter='a';
+       String Cadena="Hola mundo";
+       boolean condicion=false;
+        System.out.println("Valor Int:"+Entero);
+        System.out.println("Valor Float:"+Decimal1);
+        System.out.println("Valor Double:"+Decimal2);
+        System.out.println("Valor Char:"+caracter);
+        System.out.println("Valor String:"+Cadena);
+        System.out.println("Valor Boolean:"+condicion);*/
+       //Entrada de datos
+       /*int Entero=0;
+       float Decimal1=0;
+       double Decimal2=0;
+       char caracter;
+       String Cadena="";
+       boolean condicion;
+       Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingrese un numero:");
+        Entero=entrada.nextInt();entrada.nextLine();
+        System.out.println("Ingrese un numero float:");
+        Decimal1=entrada.nextFloat();entrada.nextLine();
+        System.out.println("Ingrese un numero double:");
+        Decimal2=entrada.nextDouble();entrada.nextLine();
+        System.out.println("Ingrese un caracter:");
+        caracter=entrada.nextLine().charAt(0);//Hola mundo->H
+        System.out.println("Ingrese un String:");
+        Cadena=entrada.nextLine();
+        System.out.println("Ingrese un boolean:");
+        condicion=entrada.nextBoolean();
+        System.out.println("El numero ingresado es:"+Entero);
+        System.out.println("El float ingresado es:"+Decimal1);
+        System.out.println("El double ingresado es:"+Decimal2);
+        System.out.println("El caracter ingresado es:"+caracter);
+        System.out.println("El String ingresado es:"+Cadena);
+        System.out.println("El boolean ingresado es:"+condicion);*/
+       
+       //condicionales -> if - switch
+       //Operadores Aritmaticos y Relacionales
+       /*
+       +,-,*,/,%,==,&&,||,!,!=,+=,-=,*=,/=,<,>,<=,>=,++,--
+       */
+       
+    }
+    
+}
