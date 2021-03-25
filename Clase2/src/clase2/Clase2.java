83364f6b455eefe1d6564c348085b5e85f0b17c4
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-03-25 15:07:34 -0600

    Revert "update"
    This reverts commit e2d70a1b000860418e429d1781d0c1b3ef560917.

--- Clase2/src/clase2/Clase2.java
+++ Clase2/src/clase2/Clase2.java
@@ -15,7 +15,7 @@ public class Clase2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Operadores y condicionales for
+        //Operadores y condicionales
     }
     
 }
