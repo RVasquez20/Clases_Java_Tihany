d79483b26ffbe3765e0dad76c993d39e055e1056
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-03-26 16:01:28 -0600

    Clase 3

---
+++ Clase 3/src/clase/pkg3/Clase3.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg3;
+
+import java.util.*;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Clase3 {
+
+    /** For
+     * Arrays->Matrices->ArrayList->List->LinkedList->Stack->Queue->HashMap(HashTable->HashSet)
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner entrada=new Scanner(System.in);
+        /* |1|3|4|5|1|
+            0 1 2 3 4   */
+       //Declarando Array
+       //int[] numeros;
+       //inicializarlo con 4 espacios
+       //numeros=new int[4];
+       //Declarar un array con valores pre-establecidos
+       //int[] numeros2={1,2,3,4};
+       //String[] Cadenas={"Juan","Pedro","Maria"};
+       //double[] dobles={12.2,12.3,12.4,12.5};
+       //boolean[] condiciones={true,false,false,true};
+       //Character[] caracteres={'a','b','c','d'};
+//       int[] numeros=new int[5];
+//
+//       for(int i=0;i<5;i++){
+//           System.out.println("Digite el valor "+(i+1));
+//           numeros[i]=entrada.nextInt();
+//       }
+//        for (int numero : numeros) {
+//            System.out.println(numero);
+//        }
+
+//char opc;
+//do{
+//int cantidad=0;
+//        System.out.println("Ingrese la cantidad de datos que ingresara al array");
+//        cantidad=entrada.nextInt();entrada.nextLine();
+//        int numeros[]=new int[cantidad];
+//        for (int i = 0; i < numeros.length; i++) {
+//            System.out.println("Digite el valor  "+(i+1));
+//            numeros[i]=entrada.nextInt();            
+//        }
+//        for (int numero : numeros) {
+//            System.out.println(numero);
+//    }
+//        entrada.nextLine();
+//        System.out.println("------------------");
+//        System.out.println("Desea Ingresar otro array? S/N");
+//        opc=entrada.next().charAt(0);
+//}while(opc!='N');
+//En Matrices PRIMERO VAN LAS FILAS Y LUEGO LAS COLUMNAS
+//En Tareas(-3pts)
+       /*   0 1 2
+          0|1|2|3
+          1|4|5|6
+          2|7|8|9
+*/
+       //Declarando Matriz
+       //int[][] matrizUno;
+       //inicializarla con 4 espacios
+       //numeros=new int[4][4];
+       //Declarar una Matriz con valores pre-establecidos
+       //int[][] Grupos ={{1,2},{3,4},{5,6},{7,8}};
+/*
+        0 1 2
+      0|1|2|3
+      1|4|5|6
+      2|7|8|9 
+       */
+
+
+//        int[][] Matriz=new int[3][3];
+//        for (int i = 0; i < Matriz.length; i++) {
+//            for (int j = 0; j < Matriz.length; j++) {
+//                System.out.println("Digite el numero que ira en la posicion ["+i+"]["+j+"]"); 
+//                Matriz[i][j]=entrada.nextInt();
+//            }
+//        }
+//        
+//        for (int i = 0; i < Matriz.length; i++) {
+//            for (int j = 0; j < Matriz.length; j++) {
+//                System.out.print(Matriz[i][j]+"|");
+//                
+//            }
+//            System.out.println("");
+//            
+//        }
+
+//int[] numeros={1,2,3,4,5,1,3,1,32,4,6,2,3,1,2,4,4,1,3,1,23,1,2,3,1,2,1,312,3,123,123,123,1,23,123,54,35,3,645,1,46,34,0};
+//Arrays.sort(numeros);
+//        for (int numero : numeros) {
+//            System.out.println(numero);
+//        }
+        //Tarea:
+        /**
+         *Hacer un Menu con las siguientes opciones
+         *1)Ingreso de datos a un array
+         * 2)Ordenar ese array(Metodo de Burbuja)
+         * 3)Buscar un elemento dentro del array
+         * 4)Mostrar el array
+         * 5)Ingreso de datos a una matriz
+         * 6)Mostrar la matriz
+         * 7)Busqueda de un elemento en la matriz
+         * 8)salir
+         */
+        /**
+         * Proyecto 1:
+         * 6/abril
+         * Hacer un juego de Tic tac toe
+         * un sistema de control de ventas con arrays colas y objetos
+         * clase Cliente,Clase Producto
+         * Muestre al finalizar una factura con la compra del cliente al que se le cobro
+         * Manejo de resticciones(No puede ser mayor el total a pagar que lo que el cliente paga)
+         * dar cambio al cliente
+         * almacenar los cambios dados 
+         * mostrar el total de ganancias del dia.
+         */
+    }
+    
+}
